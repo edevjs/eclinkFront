c2af124e4083cbe4322f165a73c04902587d5340
edevjs <68160908+edevjs@users.noreply.github.com>
2024-10-22 14:06:16 +0000

    cambio url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://eclink2.herokuapp.com/api'
+  //base_url: 'https://eclink2.herokuapp.com/api'
+  base_url: 'https://eclinkv.vercel.app/'
 };
